aa5a20d544fb6a84dea2412976d7e6e2d70b5c85
SanctusDeus <megorkin@gmail.com>
2018-10-09 14:39:58 +0300

    1

--- HM_1_-normal-2.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Введите а - "))
-b = int(input("Введите b - "))
-
-a = a + b
-b = a - b
-a = a - b
-
-print ("a = ", a)
-print ("b = ", b)
--- HM_1_basic-2.py
+++
@@ -1,8 +0,0 @@
-a = input("Введите a - ")
-b = input("Введите b - ")
-
-c = a
-a = b
-b = c
-
-print ("a = ", a, "b = ", b)
